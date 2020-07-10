2d035a920f795dd690e139e9972e3a199c7f123d
evertcolombia <evertescalante@gmail.com>
2020-07-10 16:06:06 -0500

    updatin model schema for review class in db and setting foreign keys to relationships one to many between place->reviews user->reviews

--- models/place.py
+++ models/place.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.review import Review
 from sqlalchemy import Column, ForeignKey, Float, Integer, String
-
+from sqlalchemy.orm import relationship
+import os
 
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
+
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
@@ -18,15 +21,32 @@ class Place(BaseModel, Base):
     latitude = Column(Float)
     longitude = Column(Float)
 
+    if os.environ.get("HBNB_TYPE_STORAGE") == 'db':
+        reviews = relationship('Review', backref='place', cascade='delete')
+    else:
+        """
+        It will be the FileStorage relationship between
+        Place and Review
+        """
+        @property
+        def reviews(self):
+            from models.storage import storage
+            reviews_list = []
+            objects = storage.all(Review)
+                
+            for el in objects.values():
+                if el.place_id == self.id:
+                    reviews_list.append(el)
+            return reviews_list
 
-    """city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []"""
+        """city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []"""
