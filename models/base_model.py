0958a6f3df44bfad8d7d02b892a7cb008bc3c2b3
evertcolombia <evertescalante@gmail.com>
2020-07-10 12:16:05 -0500

    updating place mapper class on db and also one-to-many relationship between user->place city->place

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 from datetime import datetime
+import models
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime
 import uuid
@@ -52,7 +53,7 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-
+        print("hello")
         if "_sa_instance_state" in dictionary:
             del dictionary['_sa_instance_state']
         return dictionary
